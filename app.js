c25d73e057ff59cb02dc48ee57c9f3bdfd22fb91
medhaholla <holla.medha@gmail.com>
2022-06-19 17:12:59 +0530

    input output processing

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-var username = prompt("Tell me your name");
-alert("My name is" + username);
+var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#text-input");
+// console.log(textInput);
+var textOutput = document.querySelector("#text-output");
+
+function clickHandler() {
+  console.log("clicked");
+  console.log("Hey there " + textInput.value);
+  textOutput.innerText = "hjhhuf" + textInput.value;
+}
+btnTranslate.addEventListener("click", clickHandler);
